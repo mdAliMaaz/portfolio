f5dde6fafe60e54f6eecbff3569c650a350e1811
ItsMaaz <130007307+mdAliMaaz@users.noreply.github.com>
2023-08-21 12:13:08 +0530

    changes

--- src/components/header/Socials.tsx
+++ src/components/header/Socials.tsx
@@ -5,7 +5,10 @@ import { FaGithubSquare } from "react-icons/fa";
 const Socials = () => {
   return (
     <div className='header__social'>
-      <a href='https://www.linkedin.com/in/md-maaz-ba0935272/' target='_blank'>
+      <a
+        href='https://www.linkedin.com/in/mohammad-ali-maaz-79b637289/'
+        target='_blank'
+      >
         <AiFillLinkedin />
       </a>
       <a href='https://github.com/mdAliMaaz' target='_blank'>
