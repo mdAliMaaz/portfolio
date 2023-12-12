6d9806359a6ed8322856051a51a36e838498e1ce
Maaz <alimaazali6@gmail.com>
2023-12-13 00:07:00 +0530

    new Resume

--- src/components/header/CTA.tsx
+++ src/components/header/CTA.tsx
@@ -1,30 +1,30 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion'
 
 const CTA = () => {
-  return (
-    <div className='cta'>
-      <motion.a
-        initial={{ y: 0 }}
-        animate={{ y: [0, -20, 0] }}
-        transition={{ duration: 1, repeat: Infinity }}
-        viewport={{ once: false }}
-        href='/Resume(Dark).pdf'
-        download
-        className='btn'
-      >
-        Download CV
-      </motion.a>
-      <motion.a
-        initial={{ y: -20 }}
-        animate={{ y: [-20, 0, -20] }}
-        transition={{ duration: 1, repeat: Infinity }}
-        href='#contact'
-        className='btn btn-primary'
-      >
-        Let's Talk
-      </motion.a>
-    </div>
-  );
-};
+	return (
+		<div className='cta'>
+			<motion.a
+				initial={{ y: 0 }}
+				animate={{ y: [0, -20, 0] }}
+				transition={{ duration: 1, repeat: Infinity }}
+				viewport={{ once: false }}
+				href='/Maaz.pdf'
+				download
+				className='btn'
+			>
+				Download CV
+			</motion.a>
+			<motion.a
+				initial={{ y: -20 }}
+				animate={{ y: [-20, 0, -20] }}
+				transition={{ duration: 1, repeat: Infinity }}
+				href='#contact'
+				className='btn btn-primary'
+			>
+				Let's Talk
+			</motion.a>
+		</div>
+	)
+}
 
-export default CTA;
+export default CTA
